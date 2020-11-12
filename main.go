a7c9ddd58ca2a471abfcfd67de561c1c09fe84b8
Jason McCarver <slam@parasite.cc>
2020-11-12 11:15:51 -0600

    Add a bit of sanity checking / data validation

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 func main() {}
 
-func UsedSlots(s string, startPositions []int, endPositions []int) []int {
-	ret := make([]int, len(startPositions))
+type slotInfo struct {
+	start int // index of starting '|' for this slot (1-based)
+	end   int // index of ending '|' for this slot
+	items int // number of '*'s in this slot
+}
 
-	for slot := range parser(s) {
-		for i := 0; i < len(startPositions); i++ {
+func UsedSlots(shelf string, startPositions []int, endPositions []int) []int {
+	// Do a bit of sanity checking in case we got mismatched position data
+	l := len(startPositions)
+	if len(endPositions) < l {
+		l = len(endPositions)
+	}
+
+	ret := make([]int, l)
+
+	for slot := range parser(shelf) {
+		for i := 0; i < l; i++ {
 			if startPositions[i] >= slot.end || startPositions[i] > slot.start {
 				continue
 			}
@@ -21,24 +33,19 @@ func UsedSlots(s string, startPositions []int, endPositions []int) []int {
 	return ret
 }
 
-type slotInfo struct {
-	start int // index of starting '|' for this slot (1-based)
-	end   int // index of ending '|' for this slot
-	items int // number of '*'s in this slot
-}
-
 // Parse input string into slots and send each slot over the returned channel
-// Only returns slots that have a beginning and ending '|'
-func parser(s string) <-chan slotInfo {
+// Only returns slots that have a beginning and ending '|' and at least one '*'
+func parser(shelf string) <-chan slotInfo {
 	ch := make(chan slotInfo)
 
 	go func() {
 		var prev, nr int
 
-		for i := 0; i < len(s); i++ {
-			switch s[i] {
+		for i := 0; i < len(shelf); i++ {
+			switch shelf[i] {
 			case '|':
-				if prev > 0 { // we have a complete slot
+				if prev > 0 && nr > 0 {
+					// we have a complete slot with at least one item
 					ch <- slotInfo{start: prev, end: i + 1, items: nr}
 				}
 				nr = 0
